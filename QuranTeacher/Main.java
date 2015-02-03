7862f6be44a521e242a01152956f0cbf2e400ecf
FazleRahat <fr.rahat@gmail.com>
2015-02-04 00:37:14 +0600

    Create Main.java

---
+++ QuranTeacher/Main.java
@@ -0,0 +1,54 @@
+/*
+ *               In the name of Allah
+ * This file is part of The "Quran Teacher or Learn Arabic" Project. Use is subject to
+ * license terms.
+ *
+ * @author:         Fazle Rabbi Rahat
+ * 
+ */
+package QuranTeacher;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	/**
+	 * Contains the main function
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Main() {
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//setDesign("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+					MainFrame frame = new MainFrame();
+					frame.pack();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+/*    private static void setDesign(String newLookAndFeel)
+	{
+		try
+		{
+			UIManager.setLookAndFeel(newLookAndFeel);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Unable to load look and feel");
+		}
+	}*/
+
+}
