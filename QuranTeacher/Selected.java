be3072b4d54fb059ce78c0b90417ac83ee9d3440
FazleRahat <fr.rahat@gmail.com>
2015-02-04 00:41:00 +0600

    Create Selected.java

---
+++ QuranTeacher/Selected.java
@@ -0,0 +1,25 @@
+/*
+ *               In the name of Allah
+ * This file is part of The "Quran Teacher or Learn Arabic" Project. Use is subject to
+ * license terms.
+ *
+ * @author:         Fazle Rabbi Rahat
+ * 
+ */
+package QuranTeacher;
+
+public class Selected {
+	public int suraIndex;
+	public int ayahIndex;
+	
+	public Selected()
+	{
+		suraIndex=0;
+		ayahIndex=0;
+	}
+	public Selected(int suraIndex,int ayahIndex)
+	{
+		this.suraIndex=suraIndex;
+		this.ayahIndex=ayahIndex;
+	}
+}
